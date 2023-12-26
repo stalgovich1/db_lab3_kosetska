c217dd82ce28e3f121c6795beb1d55bee25ca595
stalgovich1 <91222399+stalgovich1@users.noreply.github.com>
2023-12-26 11:15:54 +0200

    Update query.sql
    вибачте, я перевіряла і помітила, що не нормалізувала табличку

--- query.sql
+++ query.sql
@@ -1,16 +1,17 @@
--- Запит на вибірку загального рейтингу (weekly_rank) для певного фільму за всі країни:
+-- Загальний рейтинг (weekly_rank) для фільму "Red Notice" за всі країни
+SELECT ac.show_title, ac.country_name, ag.weekly_rank FROM  all_weeks_countries ac
+JOIN all_weeks_global ag ON ac.show_title = ag.show_title
+WHERE ac.show_title = 'Red Notice'
+UNION ALL
+
+-- Кількість годин перегляду для кожного фільму в Аргентині
+SELECT ac.show_title, ac.country_name, mp.hours_viewed_first_28_days
+FROM  all_weeks_countries ac
+JOIN most_popular mp ON ac.show_title = mp.show_title
+WHERE ac.country_name = 'Argentina'
+UNION ALL
+
+-- Загальний рейтинг (weekly_rank) для фільму за всі країни
 SELECT ac.show_title, ac.country_name
 FROM all_weeks_countries ac
 JOIN all_weeks_global ag ON ac.show_title = ag.show_title;
-
--- Запит на вибірку кількості годин перегляду для кожного фільму в конкретній країні
-SELECT ac.show_title, ac.country_name, mp.hours_viewed_first_28_days
-FROM all_weeks_countries ac
-JOIN most_popular mp ON ac.show_title = mp.show_title
-WHERE ac.country_name = 'Argentina';
-
--- Запит на вибірку загального рейтингу (weekly_rank) для певного фільму за всі країни
-SELECT ac.show_title, ac.country_name, ag.weekly_rank
-FROM all_weeks_countries ac
-JOIN all_weeks_global ag ON ac.show_title = ag.show_title
-WHERE ac.show_title = 'Red Notice';
